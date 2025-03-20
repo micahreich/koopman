e62a82a938f351f47db3f35a0343c87925411c19
Micah Reich <micahreich02@gmail.com>
2025-03-20 17:26:55 -0400

    dmd

---
+++ koopman/simulation/animation.py
@@ -0,0 +1,85 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import os
+
+
+def interp_x_u_history(ts, xs, us, ts_query):
+    right = np.minimum(len(ts) - 1, np.searchsorted(ts, ts_query, side='right'))  # First index where times[i] > t_query
+    left = np.maximum(0, right - 1)  # Previous index
+
+    alphas = (ts_query - ts[left]) / (ts[right] - ts[left])
+
+    x_query = xs[left] + alphas[:, np.newaxis] * (xs[right] - xs[left])
+
+    us_left = np.maximum(0, np.minimum(len(us) - 1, left))
+    us_right = np.maximum(0, np.minimum(len(us) - 1, right))
+
+    u_query = us[us_left] + alphas[:, np.newaxis] * (us[us_right] - us[us_left])
+
+    return ts_query, x_query, u_query
+
+
+class PlotEnvironment:
+
+    def __init__(self, fig, ax):
+        self.fig = fig
+        self.ax = ax
+        self.elements = []
+
+        self.xlo, self.xhi = float('inf'), float('-inf')
+        self.ylo, self.yhi = float('inf'), float('-inf')
+        self.zlo, self.zhi = float('inf'), float('-inf')
+
+    def add_element(self, element: "PlotElement"):
+        self.elements.append(element)
+
+    def set_xlim(self, xlo, xhi):
+        self.xlo = min(xlo, self.xlo)
+        self.xhi = max(xhi, self.xhi)
+
+        self.ax.set_xlim(self.xlo, self.xhi)
+
+    def set_ylim(self, ylo, yhi):
+        self.ylo = min(ylo, self.ylo)
+        self.yhi = max(yhi, self.yhi)
+
+        self.ax.set_ylim(self.ylo, self.yhi)
+
+    def render(self, t_range, fps=30, repeat=True, save_fpath=None):
+        n_frames = int(fps * (t_range[1] - t_range[0]))
+        actual_fps = int(n_frames / (t_range[1] - t_range[0]))
+
+        t_render = np.linspace(t_range[0], t_range[1], n_frames)
+
+        def update(idx):
+            self.fig.suptitle(r"$t = {:.2f}$ s".format(t_render[idx]))
+
+            for element in self.elements:
+                element.update(t_render[idx])
+
+        ani = animation.FuncAnimation(
+            self.fig,
+            update,
+            frames=n_frames,
+            interval=1 / actual_fps * 1e3,
+            repeat=repeat,
+        )
+
+        if save_fpath is not None:
+            base, _ = os.path.splitext(save_fpath)
+            fpath = f"{base}.mp4"
+
+            print(f"Saving animation to {fpath} ...")
+            ani.save(fpath, writer=animation.FFMpegWriter(fps=actual_fps, codec="mpeg4"))
+
+        return ani
+
+
+class PlotElement:
+
+    def __init__(self, env: PlotEnvironment) -> None:
+        self.env = env
+
+    def update(self, t, x, u) -> None:
+        raise NotImplementedError
---
+++ koopman/simulation/systems.py
@@ -0,0 +1,230 @@
+import numpy as np
+from typing import Optional, Any, Tuple
+from dataclasses import dataclass
+from scipy.linalg import solve_continuous_are
+from koopman.simulation.animation import PlotElement, PlotEnvironment
+import matplotlib.pyplot as plt
+from spatialmath.base import angle_wrap
+
+
+class DynamicalSystem:
+    nx = None
+    nu = None
+
+    def __init__(self, name: Optional[str] = None, params: Optional[Any] = None) -> None:
+        self.name = name
+        self.params = params
+
+        self.t_history = self.x_history = self.u_history = None
+
+    def __init_subclass__(cls, **kwargs):
+        super().__init_subclass__(**kwargs)
+        # Check if the subclass has its own definition of 'a'
+        if cls.nx is DynamicalSystem.nx or cls.nu is DynamicalSystem.nu:
+            raise NotImplementedError(f"Class variable 'nx' and 'nu' must be overridden in {cls.__name__}")
+
+    def project_state(self, x: np.ndarray) -> np.ndarray:
+        return x
+
+    def batch_dynamics(self, x: np.ndarray, u: np.ndarray) -> np.ndarray:
+        raise NotImplementedError
+
+    def dynamics(self, x: np.ndarray, u: np.ndarray) -> np.ndarray:
+        return self.batch_dynamics(x[None, :], u[None, :])[0, ...]
+
+    def clear_history(self) -> None:
+        self.t_history = self.x_history = self.u_history = None
+
+    def set_history(self, ts, xs, us) -> None:
+        self.t_history = ts
+        self.x_history = xs
+        self.u_history = us
+
+    def get_history(self) -> Any:
+        return (
+            np.asarray(self.t_history),
+            np.asarray(self.x_history),
+            np.asarray(self.u_history),
+        )
+
+    def query_history(self, t: float) -> Tuple[np.ndarray, np.ndarray]:
+        assert self.t_history is not None, "No time history to query"
+
+        # Clamp t to the range of the time history
+        t = max(0, min(self.t_history[-1], t))
+        idx_hi = np.searchsorted(self.t_history, t)
+        idx_lo = max(0, idx_hi - 1)
+
+        # Look up indices based on time to do linear interpolation of states, controls
+        t_lo, t_hi = self.t_history[idx_lo], self.t_history[idx_hi]
+
+        if idx_lo == idx_hi:
+            alpha_lo, alpha_hi = 0.0, 1.0
+        else:
+            alpha_hi = (t - t_lo) / (t_hi - t_lo)
+            alpha_lo = 1.0 - alpha_hi
+
+        x_interp = alpha_lo * self.x_history[idx_lo] + alpha_hi * self.x_history[idx_hi]
+
+        if idx_hi >= len(self.u_history):
+            idx_lo = idx_hi = len(self.u_history) - 1
+            alpha_hi, alpha_lo = 1.0, 0.0
+
+        u_interp = alpha_lo * self.u_history[idx_lo] + alpha_hi * self.u_history[idx_hi]
+
+        return np.asarray(x_interp), np.asarray(u_interp)
+
+
+class Pendulum(DynamicalSystem):
+
+    @dataclass
+    class Params:
+        m: float  # Mass of the pendulum
+        l: float  # Length of the pendulum
+        g: float  # Gravity
+
+    nx = 2  # [theta, theta_dot]
+    nu = 1  # Torque input
+
+    def __init__(self, params: Params) -> None:
+        super().__init__("Pendulum", params)
+
+    def project_state(self, x: np.ndarray) -> np.ndarray:
+        if len(x.shape) == 2:
+            x[:, 0] = angle_wrap(x[:, 0], mode="0:2pi")
+        else:
+            x[0] = angle_wrap(x[0], mode="0:2pi")
+        return x
+
+    def batch_dynamics(self, x: np.ndarray, u: np.ndarray) -> np.ndarray:
+        N1, nx = x.shape
+        N2, nu = u.shape
+
+        assert nx == self.nx
+        assert nu == self.nu
+        assert N1 == N2  # Batch size must match
+
+        theta, theta_dot = x[:, 0, None], x[:, 1, None]  # Extract state variables
+
+        # Compute angular acceleration
+        theta_ddot = (-self.params.g / self.params.l) * np.sin(theta) + (u / (self.params.m * self.params.l ** 2))
+
+        return np.column_stack([theta_dot, theta_ddot])  # [theta_dot, theta_ddot]
+
+
+class CartPole(DynamicalSystem):
+
+    @dataclass
+    class Params:
+        m_c: float
+        m_p: float
+        l: float
+        g: float
+
+    nx = 4
+    nu = 1
+
+    def __init__(self, params: Params) -> None:
+        super().__init__("CartPole", params)
+
+    def project_state(self, x: np.ndarray) -> np.ndarray:
+        # Project the state to a suitable range if necessary
+        # For CartPole, we might want to wrap the angle theta to be within [-pi, pi]
+        if len(x.shape) == 2:
+            theta = x[:, 1]
+            x[:, 1] = np.remainder(theta, 2 * np.pi)
+        else:
+            theta = x[1]
+            x[1] = theta % (2 * np.pi)
+
+        return x
+
+    def batch_dynamics(self, x: np.ndarray, u: np.ndarray) -> np.ndarray:
+        N1, nx = x.shape
+        N2, nu = u.shape
+
+        assert nx == self.nx
+        assert nu == self.nu
+        assert N1 == N2
+
+        theta, v, theta_dot = x[:, 1, None], x[:, 2, None], x[:, 3, None]
+
+        # Compute accelerations
+        x_ddot = (1 / (self.params.m_c + self.params.m_p * np.sin(theta) ** 2)) * (
+            u + self.params.m_p * np.sin(theta) * (self.params.l * theta_dot ** 2 + self.params.g * np.cos(theta)))
+
+        theta_ddot = (1 / (self.params.l * (self.params.m_c + self.params.m_p * np.sin(theta) ** 2))) * (
+            -u * np.cos(theta) - self.params.m_p * self.params.l * theta_dot ** 2 * np.cos(theta) * np.sin(theta) -
+            (self.params.m_c + self.params.m_p) * self.params.g * np.sin(theta))
+
+        # Stack results into an output array
+        return np.column_stack([v, theta_dot, x_ddot, theta_ddot])
+
+    class PlotElement(PlotElement):
+
+        def __init__(self, env: PlotEnvironment, sys: "CartPole", cart_color='blue') -> None:
+            super().__init__(env)
+
+            self.sys = sys
+
+            self.cart_width, self.cart_height = 0.4, 0.2
+
+            self.cart = self.env.ax.add_patch(
+                plt.Rectangle((-self.cart_width / 2, -self.cart_height / 2),
+                              self.cart_width,
+                              self.cart_height,
+                              fc=cart_color))  # Cart
+            (self.rod, ) = self.env.ax.plot([], [], 'o-', lw=2, markersize=5, c='black', markerfacecolor='gray')  # Pole
+
+            x_lo, x_hi = sys.x_history[:, 0].min(), sys.x_history[:, 0].max()
+            new_range = (x_hi - x_lo + self.cart_width) * 1.2
+
+            x_lo = (x_lo + x_hi) / 2 - new_range / 2
+            x_hi = (x_lo + x_hi) / 2 + new_range / 2
+
+            self.env.set_xlim(x_lo, x_hi)
+            self.env.set_ylim(-sys.params.l * 1.2, sys.params.l * 1.2)
+
+        def update(self, t):
+            state, _ = self.sys.query_history(t)  # Get the current state of the cartpole
+            x, theta = state[0], state[1]  # Extract cart position and pole angle
+
+            # Compute the pole's end position
+            l = self.sys.params.l
+            pole_x = x + l * np.sin(theta)
+            pole_y = -l * np.cos(theta)
+
+            # Update cart position
+            self.cart.set_xy((x - 0.2, -0.1))  # Adjusted for cart size
+
+            # Update pole position
+            self.rod.set_data([x, pole_x], [0, pole_y])
+
+
+if __name__ == "__main__":
+    cart_pole = CartPole(params=CartPole.Params(1, 1, 1, 9.81))
+
+    xbar = np.array([0, np.pi - 0.4, 0, 0], dtype=np.float32)
+    ubar = np.array([24.24184], dtype=np.float32)
+
+    print(cart_pole.dynamics(xbar, ubar))
+
+    xbar = np.tile(xbar, (5, 1))
+    ubar = np.tile(ubar, (5, 1))
+
+    print(cart_pole.batch_dynamics(xbar, ubar))
+
+    # ubar = torch.tensor([0.], dtype=torch.float32)
+    # ubar = torch.tile(ubar, dims=(100, 1))
+
+    # print(cart_pole.batch_dynamics(xbar, ubar))
+    # print(cart_pole.dynamics(xbar, ubar))
+
+    # A = torch_to_numpy(torch.autograd.functional.jacobian(lambda _x : cart_pole.dynamics(_x, ubar), xbar))
+    # B = torch_to_numpy(torch.autograd.functional.jacobian(lambda _u : cart_pole.dynamics(xbar, _u), ubar))
+    # Q = np.eye(4)
+    # R = np.eye(1)
+    # P = solve_continuous_are(A, B, Q, R)
+    # K = np.linalg.inv(R) @ B.T @ P
+
+    # print(A.shape, B.shape, K.shape)
